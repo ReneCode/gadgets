1cfb2ebccab47647ebc8671d416c1bc8aa3f5af4
Rene <harrymillermail@googlemail.com>
2015-01-03 14:35:41 +0100

    object Item

--- try/animate.js
+++ try/animate.js
@@ -9,53 +9,126 @@ window.requestAnimFrame = (function(callback) {
   };
 })();
 
-function drawRectangle(myRectangle, context) {
-  context.beginPath();
-  context.rect(myRectangle.x, myRectangle.y, myRectangle.width, myRectangle.height);
-  context.fillStyle = '#8ED6FF';
-  context.fill();
-  context.lineWidth = myRectangle.borderWidth;
-  context.strokeStyle = 'black';
-  context.stroke();
-}
-function animate(myRectangle, canvas, context, startTime) {
+
+
+
+var Item = function() {
+  this.x = 0;
+  this.y = 100;
+  this.width = 40;
+  this.height = 40;
+  this.borderWidth = 8;
+  this.fillColor = "white";
+  this.borderColor = "black";
+  this.flash = {
+    pattern: "100",
+    current: 0
+  };
+};
+
+
+Item.prototype.updateMove = function(startTime, canvas) {
   // update
   var time = (new Date()).getTime() - startTime;
 
-  var linearSpeed = 10;
+  var linearSpeed = 40;
   // pixels / second
   var newX = linearSpeed * time / 1000;
+
+  console.log("NewX: " + newX);
+
+  if(newX < canvas.width - this.width - this.borderWidth / 2) {
+    this.x = newX;
+  }
+};
+
+Item.prototype.updateFlash = function() {
+  if (this.fillColor == 'blue') {
+    this.fillColor = 'red';
+  }
+  else {
+    this.fillColor = 'blue';
+  }
+};
+
+Item.prototype.draw = function(context) {
+  context.beginPath();
+  context.rect(this.x, this.y, this.width, this.height);
+  context.fillStyle = this.fillColor;
+  context.fill();
+  context.lineWidth = this.borderWidth;
+  context.strokeStyle = this.borderColor;
+  context.stroke();
+}
+
+
+var myRectangle = new Item();
+
+function animate(myRectangle, canvas, context, startTime) {
+
+
+  /*
+  // update
+  var time = (new Date()).getTime() - startTime;
+
+  var linearSpeed = 40;
+  // pixels / second
+  var newX = linearSpeed * time / 1000;
+
+  console.log("NewX: " + newX);
 
   if(newX < canvas.width - myRectangle.width - myRectangle.borderWidth / 2) {
     myRectangle.x = newX;
   }
+  else
+  {
+    return;
+  }
+*/
+
+  myRectangle.updateMove(startTime, canvas);
 
   // clear
   context.clearRect(0, 0, canvas.width, canvas.height);
 
-  drawRectangle(myRectangle, context);
+
+  myRectangle.draw(context);
 
   // request new frame
   requestAnimFrame(function() {
     animate(myRectangle, canvas, context, startTime);
   });
 }
+
+
 var canvas = document.getElementById('myCanvas');
 var context = canvas.getContext('2d');
 
-var myRectangle = {
-  x: 0,
-  y: 75,
-  width: 100,
-  height: 50,
-  borderWidth: 5
-};
-
-drawRectangle(myRectangle, context);
+//drawRectangle(myRectangle, context);
 
 // wait one second before starting animation
 setTimeout(function() {
   var startTime = (new Date()).getTime();
   animate(myRectangle, canvas, context, startTime);
 }, 1000);
+
+
+var flashInterval = setInterval( function() {
+  myRectangle.updateFlash();
+/*
+  if (myRectangle.fillColor == 'blue') {
+    myRectangle.fillColor = 'red';
+  }
+  else {
+    myRectangle.fillColor = 'blue';
+  }
+  
+  var newSize = myRectangle.width+3;
+  if (newSize > 70) {
+    newSize = 40;
+  }
+  myRectangle.height = newSize;
+  myRectangle.width = newSize;
+  */
+}, 500);
 
