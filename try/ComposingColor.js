d1620211c55102ade1727298a79e9e51e1acbd9e
HarryMiller <HarryMillerMail@googlemail.com>
2015-01-18 21:17:03 +0100

    with real colors

---
+++ try/ComposingColor.js
@@ -0,0 +1,17 @@
+
+	var getColorFromNumber = function(colNr) {
+		switch (colNr) {
+			case 1: return '#6CD5FA';
+			case 2: return '#5a98fa';
+			case 3: return '#4032e6';
+			case 4: return '#8e39ef';
+			case 5: return '#c73e87';
+			case 6: return '#db5758';
+			case 7: return '#e5734c';
+			case 8: return '#ec9f4a';
+			case 9: return '#f1b64d';
+			case 10: return '#fde77d';
+			default: return '#eeeeee';
+		}
+	};
+
